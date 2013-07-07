211873b84428d939ce6c97c41db48ca064ffb6a1
Adam Langley <agl@imperialviolet.org>
2013-07-07 12:30:35 -0400

    OS X building fixes

--- client/client.go
+++ client/client.go
@@ -21,7 +21,6 @@ import (
 	"github.com/agl/ed25519"
 	"github.com/agl/pond/bbssig"
 	"github.com/agl/pond/client/disk"
-	"github.com/agl/pond/client/tpm"
 	"github.com/agl/pond/panda"
 	pond "github.com/agl/pond/protos"
 )
@@ -2234,112 +2233,6 @@ func (c *client) createPassphraseUI() string {
 		}
 
 		return pw
-	}
-
-	panic("unreachable")
-}
-
-func (c *client) createErasureStorage(pw string, stateFile *disk.StateFile) error {
-	var tpmInfo string
-	present := tpm.Present()
-	if present {
-		tpmInfo = "Your computer appears to have a TPM chip. Click below to try and use it. You'll need tcsd (the TPM daemon) running."
-	} else {
-		tpmInfo = "Your computer does not appear to have a TPM chip. Without one, it's possible that someone in physical possession of your computer and passphrase could extract old messages that should have been deleted. Using a computer with a TPM is strongly preferable until alternatives can be implemented."
-	}
-
-	ui := VBox{
-		widgetBase: widgetBase{padding: 40, expand: true, fill: true, name: "vbox"},
-		children: []Widget{
-			Label{
-				widgetBase: widgetBase{font: "DejaVu Sans 30"},
-				text:       "Configure TPM",
-			},
-			Label{
-				widgetBase: widgetBase{
-					padding: 20,
-					font:    "DejaVu Sans 14",
-				},
-				text: "It's very difficult to erase information on modern computers so Pond tries to use the TPM chip if possible.\n\n" + tpmInfo,
-				wrap: 600,
-			},
-			HBox{
-				children: []Widget{
-					Button{
-						widgetBase: widgetBase{
-							name:        "tpm",
-							insensitive: !present,
-						},
-						text: "Try to configure TPM",
-					},
-				},
-			},
-			TextView{
-				widgetBase: widgetBase{name: "log", expand: true, fill: true},
-				editable:   false,
-			},
-			Button{
-				widgetBase: widgetBase{name: "continue"},
-				text:       "Continue without TPM",
-			},
-		},
-	}
-
-	c.ui.Actions() <- SetBoxContents{name: "body", child: ui}
-	c.ui.Actions() <- SetFocus{name: "tpm"}
-	c.ui.Actions() <- UIState{uiStateErasureStorage}
-	c.ui.Signal()
-
-	var logText string
-
-	tpm := disk.TPM{
-		Log: func(format string, args ...interface{}) {
-			c.log.Printf(format, args...)
-			logText += fmt.Sprintf(format, args...) + "\n"
-			c.ui.Actions() <- SetTextView{name: "log", text: logText}
-			c.ui.Signal()
-		},
-		Rand: c.rand,
-	}
-
-NextEvent:
-	for {
-		event, ok := <-c.ui.Events()
-		if !ok {
-			c.ShutdownAndSuspend()
-		}
-
-		click, ok := event.(Click)
-		if !ok {
-			continue
-		}
-
-		switch click.name {
-		case "continue":
-			stateFile.Erasure = nil
-			return stateFile.Create(pw)
-		case "tpm":
-			if len(logText) > 0 {
-				c.ui.Actions() <- SetTextView{name: "log", text: ""}
-				c.ui.Signal()
-				logText = ""
-				time.Sleep(300 * time.Millisecond)
-			}
-
-			stateFile.Erasure = &tpm
-			c.ui.Actions() <- Sensitive{name: "tpm", sensitive: false}
-			c.ui.Actions() <- Sensitive{name: "continue", sensitive: false}
-			c.ui.Signal()
-			if err := stateFile.Create(pw); err != nil {
-				tpm.Log("Setup failed with error: %s", err)
-				tpm.Log("You can click the button to try again")
-				c.ui.Actions() <- Sensitive{name: "tpm", sensitive: true}
-				c.ui.Actions() <- Sensitive{name: "continue", sensitive: true}
-				c.ui.Signal()
-				continue NextEvent
-			}
-			return nil
-		}
 	}
 
 	panic("unreachable")
---
+++ client/erasure_darwin.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/agl/pond/client/disk"
+)
+
+func (c *client) createErasureStorage(pw string, stateFile *disk.StateFile) error {
+	// No NVRAM support on OS X yet.
+	return nil
+}
