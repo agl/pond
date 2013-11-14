148066d4ab710b1121f52dd88fa8921665d26f2d
Arlo Breault <arlolra@gmail.com>
2013-11-14 11:10:35 -0800

    Add Info.plist from #18.
    Fixes the flags issue where Finder passes in the process serial number.

--- client/build-osx-bundle.sh
+++ client/build-osx-bundle.sh
@@ -1,9 +1,14 @@
+#!/bin/sh
+
 rm -Rf Pond.app
-mkdir -p Pond.app/Contents/MacOS
-frameworks=Pond.app/Contents/Frameworks
+contents=Pond.app/Contents
+mac=$contents/MacOS
+mkdir -p $mac
+frameworks=$contents/Frameworks
 mkdir -p $frameworks
-binary=Pond.app/Contents/MacOS/Pond
+binary=$mac/Pond
 cp -av client $binary
+cp -av Info.plist $contents/Info.plist
 
 for lib in $(otool -L ./client | grep '^\t/usr/local' | sed -e 's/^[^\/]*//' -e 's/ .*//'); do
   base=$(basename $lib)
