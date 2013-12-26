4227a1ff71bc44f152d67d125dda68d6d1272951
Adam Langley <agl@imperialviolet.org>
2013-12-26 10:23:38 -0500

    Get CLI working on OS X.

--- client/cli.go
+++ client/cli.go
@@ -1,5 +1,3 @@
-// +build !darwin
-
 package main
 
 import (
@@ -21,7 +19,6 @@ import (
 	"code.google.com/p/goprotobuf/proto"
 	"github.com/agl/pond/client/disk"
 	"github.com/agl/pond/client/system"
-	"github.com/agl/pond/client/tpm"
 	"github.com/agl/pond/panda"
 	pond "github.com/agl/pond/protos"
 )
@@ -391,56 +388,6 @@ func (c *cliClient) createPassphraseUI() (string, error) {
 		c.Printf("%s Passphrases don't match. Please start over\n", termInfoPrefix)
 	}
 	return "", nil
-}
-
-func (c *cliClient) createErasureStorage(pw string, stateFile *disk.StateFile) error {
-	c.Printf("%s %s\n", termInfoPrefix, tpmIntroMsg)
-
-	present := tpm.Present()
-	if !present {
-		c.Printf("%s\n", termErrPrefix, tpmNotPresentMsg)
-	} else {
-		c.Printf("%s %s\n", termInfoPrefix, tpmPresentMsg)
-	}
-
-ConfigureLoop:
-	for present {
-		c.term.SetPrompt("Try to configure TPM (y/n)> ")
-
-	PromptLoop:
-		for {
-			line, err := c.term.ReadLine()
-			if err != nil {
-				return err
-			}
-			switch line {
-			case "y", "yes", "Yes":
-				break PromptLoop
-			case "n", "no", "No":
-				break ConfigureLoop
-			}
-		}
-
-		tpm := disk.TPM{
-			Log: func(format string, args ...interface{}) {
-				msg := fmt.Sprintf(format, args...)
-				c.Printf("%s\n", terminalEscape(msg, false))
-			},
-			Rand: c.rand,
-		}
-
-		stateFile.Erasure = &tpm
-		if err := stateFile.Create(pw); err != nil {
-			c.Printf("%s Setup failed with error: %s\n", termErrPrefix, terminalEscape(err.Error(), false))
-		} else {
-			c.Printf("%s TPM in use for this statefile\n", termInfoPrefix)
-			return nil
-		}
-	}
-
-	c.Printf("%s TPM will not be used for this statefile\n", termErrPrefix)
-	stateFile.Erasure = nil
-	return stateFile.Create(pw)
 }
 
 func (c *cliClient) createAccountUI() error {
